627fd6bc6b695863a7dbbb3886e78e9366b107eb
Sergio Lavor <127288387+prof-sergio-lavor@users.noreply.github.com>
2025-03-07 08:26:11 -0300

    1 versão

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
+  </head>
+  <body>
+    <div class="container">
+        <div class="coluna">
+            <div class="linha">
+            <h1>Cadastre-se</h1>
+            
+    <form>
+  <div class="mb-3">
+    <label for="nome" class="form-label">Nome Completo</label>
+    <input type="text" class="form-control" id="nome">
+  
+ 
+    <label for="endereco" class="form-label">Endereço</label>
+    <input type="text" class="form-control" id="endereco">
+  
+  
+    <label for="telefone" class="form-label">Telefone</label>
+    <input type="text" class="form-control" id="telefone">
+
+    <label for="email" class="form-label">E-mail</label>
+    <input type="email" class="form-control" id="email">
+
+    <label for="data" class="form-label">Data de Nascimento</label>
+    <input type="date" class="form-control" id="data">
+
+
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    
+
+
+  </body>
+</html>
